b25fb40039b971cc0ad2395c6de2f28e38e11e00
nguyenthetuan <tuannghuyenthetuan@gmail.com>
2020-02-05 11:30:44 +0700

    test one

--- test.js
+++ test.js
@@ -2,5 +2,6 @@ var http = require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.writeHead(200,{'Content-Type': 'text/plan'});
   res.end('Hello World!');
 }).listen(8080);
